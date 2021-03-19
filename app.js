455e5d4ec0c3cedd3b636bb4b3a6c67c4e6afd64
Aanandan-T-Ma <aanandan.tma@gmail.com>
2021-03-19 12:30:05 +0530

    db connected

--- app.js
+++ app.js
@@ -3,11 +3,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+const connect = mongoose.connect('mongodb://localhost:27017/MedBuddy')
+
+connect.then((db) => {
+    console.log("\nDatabase connected!")
+}, (err) => console.log(err))
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
