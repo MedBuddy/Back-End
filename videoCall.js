2877167fe37ce97412e69f9fd9a9e64bcdf725de
Aanandan-T-Ma <aanandan.tma@gmail.com>
2021-05-14 21:06:50 +0530

    Video Call server

---
+++ videoCall.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+
+const http = require('http')
+const server = http.createServer(app)
+const PORT = 5000
+
+const io = require('socket.io')(server, {
+    cors: {
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST']
+    }
+})
+
+io.on('connection', socket => {
+    socket.emit('myId', socket.id)
+
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('callEnded')
+    })
+
+    socket.on('call-user', data => {
+        io.to(data.userToCall).emit('call-user', {
+            signal: data.signalData,
+            from: data.from,
+            name: data.name
+        })
+    })
+
+    socket.on('answer-call', data => {
+        io.to(data.to).emit('callAccepted', data.signal)
+    })
+})
+
+server.listen(PORT, () => console.log(`Server listening on Port ${PORT}`))
